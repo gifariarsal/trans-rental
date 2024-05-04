fd15aaaeee5581ba86da6ba2f471f02f9485cc0a
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-05-04 09:42:34 +0800

    style: update box-shadow style for HowItWorksItem
    - modify the box-shadow styling on HowItWorksItem components to enhance visual depth and improve overall aesthetic appeal

--- src/components/landing/HowItWorksItem.jsx
+++ src/components/landing/HowItWorksItem.jsx
@@ -6,7 +6,7 @@ const HowItWorksItem = ({ icon, text, className = '' }) => {
       className="w-full md:w-[250px] flex flex-row md:flex-col items-center justify-start md:justify-center gap-4 mx-auto p-4 rounded-2xl border-[1px] border-black/70 md:hover:cursor-pointer md:hover:scale-105 transition-all duration-300"
       style={{
         boxShadow:
-          '0 8px 16px rgba(0,0,0,0.05), 0 -8px 16px rgba(0,0,0,0.05), -8px 0 16px rgba(0,0,0,0.05), 8px 0 16px rgba(0,0,0,0.05)',
+          '-3px -3px 12px 0 rgba(236, 236, 236, 0.09), 3px 3px 12px 0 rgba(0, 0, 0, 0.59)',
       }}
     >
       <div
